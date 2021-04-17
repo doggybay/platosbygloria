10365200aecacf7edaf419ac2b35a7651bd783e6
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-17 11:08:57 -0700

    creating plates

---
+++ src/assets/data/plates.js
@@ -0,0 +1,22 @@
+import * as pictures from "../pictures/pictures";
+
+export const plates = [
+  {
+    id: 1,
+    name: "molletes",
+    description: "pan dulce con dulce",
+    picture: pictures.molletes,
+  },
+  {
+    id: 2,
+    name: "tacos",
+    description: "tortilla de maiz con carne de res",
+    picture: pictures.tacos,
+  },
+  {
+    id: 3,
+    name: "pescado con arroz",
+    description: "pescado y arroze blanco",
+    picture: pictures.pescadoConArroz,
+  },
+];
