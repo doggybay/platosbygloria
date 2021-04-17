b667710d20ce34f80a0a17556bf1ae74da8c0e57
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-17 11:10:32 -0700

    adding plates

--- src/App.js
+++ src/App.js
@@ -1,32 +1,17 @@
 import React from 'react';
-import './App.css';
+// import './App.css';
 import AppBar from "@material-ui/core/AppBar";
 import Button from "@material-ui/core/Button";
-import CameraIcon from "@material-ui/icons/PhotoCamera";
-import Card from "@material-ui/core/Card";
-import CardActions from "@material-ui/core/CardActions";
-import CardContent from "@material-ui/core/CardContent";
-import CardMedia from "@material-ui/core/CardMedia";
+import FastfoodIcon from "@material-ui/icons/Fastfood";
 import CssBaseline from "@material-ui/core/CssBaseline";
 import Grid from "@material-ui/core/Grid";
 import Toolbar from "@material-ui/core/Toolbar";
 import Typography from "@material-ui/core/Typography";
 import { makeStyles } from "@material-ui/core/styles";
 import Container from "@material-ui/core/Container";
-import Link from "@material-ui/core/Link";
+import Plate from "./components/plates/Plate";
+import { plates } from "./assets/data/plates";
 
-function Copyright() {
-  return (
-    <Typography variant="body2" color="textSecondary" align="center">
-      {"Copyright © "}
-      <Link color="inherit" href="https://material-ui.com/">
-        Your Website
-      </Link>{" "}
-      {new Date().getFullYear()}
-      {"."}
-    </Typography>
-  );
-}
 
 const useStyles = makeStyles((theme) => ({
   icon: {
@@ -60,8 +45,6 @@ const useStyles = makeStyles((theme) => ({
   },
 }));
 
-const cards = [1, 2, 3, 4, 5, 6, 7, 8, 9];
-
 function App() {
   const classes = useStyles();
 
@@ -70,9 +53,9 @@ function App() {
       <CssBaseline />
       <AppBar position="relative">
         <Toolbar>
-          <CameraIcon className={classes.icon} />
+          <FastfoodIcon className={classes.icon} />
           <Typography variant="h6" color="inherit" noWrap>
-            Album layout
+            Platos by Gloria
           </Typography>
         </Toolbar>
       </AppBar>
@@ -87,7 +70,7 @@ function App() {
               color="textPrimary"
               gutterBottom
             >
-              Album layout
+              Platos by Gloria
             </Typography>
             <Typography
               variant="h5"
@@ -95,21 +78,15 @@ function App() {
               color="textSecondary"
               paragraph
             >
-              Something short and leading about the collection below—its
-              contents, the creator, etc. Make it short and sweet, but not too
-              short so folks don&apos;t simply skip over it entirely.
+              Authentica Cocina Guatemanteca
             </Typography>
             <div className={classes.heroButtons}>
               <Grid container spacing={2} justify="center">
                 <Grid item>
-                  <Button variant="contained" color="primary">
-                    Main call to action
-                  </Button>
+                  <Typography>818-618-1448</Typography>
                 </Grid>
                 <Grid item>
-                  <Button variant="outlined" color="primary">
-                    Secondary action
-                  </Button>
+                  <Typography>example@example.com</Typography>
                 </Grid>
               </Grid>
             </div>
@@ -118,33 +95,8 @@ function App() {
         <Container className={classes.cardGrid} maxWidth="md">
           {/* End hero unit */}
           <Grid container spacing={4}>
-            {cards.map((card) => (
-              <Grid item key={card} xs={12} sm={6} md={4}>
-                <Card className={classes.card}>
-                  <CardMedia
-                    className={classes.cardMedia}
-                    image="https://source.unsplash.com/random"
-                    title="Image title"
-                  />
-                  <CardContent className={classes.cardContent}>
-                    <Typography gutterBottom variant="h5" component="h2">
-                      Heading
-                    </Typography>
-                    <Typography>
-                      This is a media card. You can use this section to describe
-                      the content.
-                    </Typography>
-                  </CardContent>
-                  <CardActions>
-                    <Button size="small" color="primary">
-                      View
-                    </Button>
-                    <Button size="small" color="primary">
-                      Edit
-                    </Button>
-                  </CardActions>
-                </Card>
-              </Grid>
+            {plates.map((plate) => (
+              <Plate plate={plate} />
             ))}
           </Grid>
         </Container>
@@ -152,7 +104,7 @@ function App() {
       {/* Footer */}
       <footer className={classes.footer}>
         <Typography variant="h6" align="center" gutterBottom>
-          Footer
+          Platos by Gloria
         </Typography>
         <Typography
           variant="subtitle1"
@@ -160,9 +112,8 @@ function App() {
           color="textSecondary"
           component="p"
         >
-          Something here to give the footer a purpose!
+          Thanks for stopping by.
         </Typography>
-        <Copyright />
       </footer>
       {/* End footer */}
     </React.Fragment>
