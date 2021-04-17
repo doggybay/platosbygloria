20b7b1313d8d3547b12e93320ea05afb4b9c6b33
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-17 11:10:10 -0700

    exporting pictures

---
+++ src/assets/pictures/pictures.js
@@ -0,0 +1,11 @@
+import molletes from "../pictures/molletes.png";
+import tacos from "../pictures/tacos.png";
+import pescadoConArroz from "../pictures/pescadoconarroz.png";
+
+
+
+export {
+  molletes,
+  tacos,
+  pescadoConArroz,
+}
