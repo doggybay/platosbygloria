aa1485a66c824579a9094b6504d56216d2f6829e
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-21 13:08:04 -0700

    creating theme

---
+++ src/theme/theme.js
@@ -0,0 +1,17 @@
+import { createMuiTheme } from "@material-ui/core/styles";
+
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: "#31525b",
+    },
+    secondary: {
+      main: "#b3dee5",
+    },
+    background: {
+      default: "#b3dee5",
+    },
+  },
+});
+
+export default theme;
