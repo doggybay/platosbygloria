cddd44014f7099281e8c2bd63abe102f175e160b
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-21 13:07:53 -0700

    adding theme provider

--- src/App.js
+++ src/App.js
@@ -1,18 +1,22 @@
 import React from 'react';
 import CssBaseline from "@material-ui/core/CssBaseline";
+import { ThemeProvider } from "@material-ui/core/styles";
 import NavBar from "./components/layout/NavBar";
 import Footer from "./components/layout/Footer";
 import Content from "./components/content/Content";
+import theme from "./theme/theme";
 
 
 function App() {
 
   return (
     <React.Fragment>
-      <CssBaseline />
-      <NavBar />
-      <Content />
-      <Footer />
+      <ThemeProvider theme={theme}>
+        <CssBaseline />
+        <NavBar />
+        <Content />
+        <Footer />
+      </ThemeProvider>
     </React.Fragment>
   );
 }
