089a3e37e2b463e7c075fe93311fca2332b0d58e
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-04-21 13:17:47 -0700

    added note for colors

--- src/theme/theme.js
+++ src/theme/theme.js
@@ -1,5 +1,13 @@
 import { createMuiTheme } from "@material-ui/core/styles";
 
+/*
+Theme color palette
+dark blue: "#31525b"
+dark orange: "#ffa101"
+light blue: "#b3dee5"
+light orange: "#fae6b1"
+*/
+
 const theme = createMuiTheme({
   palette: {
     primary: {
