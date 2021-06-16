f6a6564bef72b4300ce8ff5234711f0698b6f520
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-06-15 19:12:56 -0700

    updating pictures

--- src/assets/pictures/pictures.js
+++ src/assets/pictures/pictures.js
@@ -1,6 +1,9 @@
 import molletes from "../pictures/molletes.png";
 import tacos from "../pictures/tacos.png";
 import pescadoConArroz from "../pictures/pescadoconarroz.png";
+import platosByGloriaLogo1 from "../pictures/platos_by_gloria_logo_1.png";
+import platosByGloriaLogo2 from "../pictures/platos_by_gloria_logo_2.png";
+import platosByGloriaLogo3 from "../pictures/platos_by_gloria_logo_3.png";
 
 
 
@@ -8,4 +11,7 @@ export {
   molletes,
   tacos,
   pescadoConArroz,
-}
+  platosByGloriaLogo1,
+  platosByGloriaLogo2,
+  platosByGloriaLogo3,
+};
