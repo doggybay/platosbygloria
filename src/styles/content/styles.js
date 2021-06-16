e43d0d480832caaed1675dbdbf5fe679a0734311
Julio Miranda <48833960+doggybay@users.noreply.github.com>
2021-06-15 19:15:31 -0700

    adding styles

--- src/styles/content/styles.js
+++ src/styles/content/styles.js
@@ -1,11 +1,18 @@
 import { makeStyles } from "@material-ui/core/styles";
+import * as pictures from "../../assets/pictures/pictures";
 
 export const contentStyles = makeStyles((theme) => ({
   heroContent: {
-    backgroundColor: theme.palette.background.paper,
+    // backgroundColor: theme.palette.background.paper,
+    
     padding: theme.spacing(8, 0, 6),
   },
   heroButtons: {
     marginTop: theme.spacing(4),
+  },
+  logo: {
+    backgroundImage: `url(${pictures.platosByGloriaLogo3})`,
+    height: "500px"
+    
   },
 }));
