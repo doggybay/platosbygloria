da35b8b8ac82ee025ec155cb6a9ef96e31bfc0e4
Julio Miranda <julio.miranda12@gmail.com>
2021-09-19 15:18:57 -0700

    updating picture source

--- src/assets/data/plates.js
+++ src/assets/data/plates.js
@@ -1,22 +1,21 @@
-import * as pictures from "../pictures/pictures";
 
 export const plates = [
   {
     id: 1,
     name: "molletes",
     description: "pan dulce con dulce",
-    picture: pictures.molletes,
+    picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088621/plates/molletes_bjahmu.jpg",
   },
   {
     id: 2,
     name: "tacos",
     description: "tortilla de maiz con carne de res",
-    picture: pictures.tacos,
+    picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088618/plates/tacos_c7t3mu.jpg",
   },
   {
     id: 3,
     name: "pescado con arroz",
     description: "pescado y arroze blanco",
-    picture: pictures.pescadoConArroz,
+    picture: "https://res.cloudinary.com/platosbygloria/image/upload/v1632088618/plates/pescadoconarroz_ax1co4.jpg",
   },
 ];
