462a6d3a34af2cca58d44a6f5ab9d320d2bd92ba
Julio Miranda <julio.miranda12@gmail.com>
2021-09-19 15:18:30 -0700

    updating logo picture

--- src/components/content/BizName.js
+++ src/components/content/BizName.js
@@ -8,14 +8,7 @@ import * as pictures from "../../assets/pictures/pictures";
 const BizName = ({ classes }) => {
   return (
     <Card>
-      <CardMedia className={classes.logo} src={pictures.platosByGloriaLogo1} />
-      {/* <Typography
-        component="h1"
-        variant="h2"
-        align="center"
-        color="textPrimary"
-        gutterBottom
-      ></Typography> */}
+      <CardMedia className={classes.logo} src={'https://res.cloudinary.com/platosbygloria/image/upload/v1632088646/media/platos_by_gloria_logo_3_bhp3mb.png'} />
     </Card>
   );
 }
