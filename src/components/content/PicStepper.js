38289e0194090c011aa82751ef90726927623fca
Julio Miranda <julio.miranda12@gmail.com>
2021-10-05 21:54:38 -0700

    pic stepper base

---
+++ src/components/content/PicStepper.js
@@ -0,0 +1,86 @@
+import { useState } from 'react';
+import { useTheme } from '@mui/material/styles';
+import Box from '@mui/material/Box';
+import MobileStepper from '@mui/material/MobileStepper';
+import Paper from '@mui/material/Paper';
+import Typography from '@mui/material/Typography';
+import Button from '@mui/material/Button';
+import KeyboardArrowLeft from '@mui/icons-material/KeyboardArrowLeft';
+import KeyboardArrowRight from '@mui/icons-material/KeyboardArrowRight';
+import SwipeableViews from 'react-swipeable-views';
+import { autoPlay } from 'react-swipeable-views-utils';
+import { plates } from '../../assets/data/plates';
+import Theme from '../../theme/theme';
+
+const AutoPlaySwipeableViews = autoPlay(SwipeableViews);
+
+const images = plates;
+
+function SwipeableTextMobileStepper() {
+  const theme = useTheme(Theme);
+  const [activeStep, setActiveStep] = useState(0);
+  const maxSteps = images.length;
+
+  const handleNext = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep + 1);
+  };
+
+  const handleBack = () => {
+    setActiveStep((prevActiveStep) => prevActiveStep - 1);
+  };
+
+  const handleStepChange = (step) => {
+    setActiveStep(step);
+  };
+
+  return (
+    <Box sx={{ maxWidth: 320, flexGrow: 1 }}>
+      <Paper
+        square
+        elevation={0}
+        sx={{
+          display: 'flex',
+          alignItems: 'center',
+          height: 20,
+          pl: 2,
+          bgcolor: 'background.default',
+        }}
+      >
+        
+      </Paper>
+      <AutoPlaySwipeableViews
+        axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+        index={activeStep}
+        onChangeIndex={handleStepChange}
+        enableMouseEvents
+      >
+        {images.map((step, index) => (
+          <div key={step.label}>
+            {Math.abs(activeStep - index) <= 2 ? (
+              <Box
+                component="img"
+                sx={{
+                  height: 300,
+                  display: 'block',
+                  maxWidth: 320,
+                  overflow: 'hidden',
+                  width: '100%',
+                }}
+                src={step.picture}
+                alt={step.label}
+              />
+            ) : null}
+          </div>
+        ))}
+      </AutoPlaySwipeableViews>
+      <MobileStepper
+        steps={maxSteps}
+        position="static"
+        activeStep={activeStep}
+        
+      />
+    </Box>
+  );
+}
+
+export default SwipeableTextMobileStepper;
