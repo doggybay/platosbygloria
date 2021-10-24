fc20e27b36668dc0f632ea5ca8069b6632b59b45
Julio Miranda <julio.miranda12@gmail.com>
2021-10-24 23:15:09 +0100

    updating firebase config

--- src/index.js
+++ src/index.js
@@ -3,35 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
+import { initializeApp } from 'firebase/app';
+
 import { ThemeProvider } from '@material-ui/core/styles';
 import { Provider } from 'react-redux';
+
 import store from '../src/store';
 import theme from './theme/theme';
 
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getFirestore } from "firebase/firestore";
-
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
-const firebaseConfig = {
-  apiKey: process.env.REACT_APP_apiKey,
-  authDomain: process.env.REACT_APP_authDomain,
-  projectId: process.env.REACT_APP_projectId,
-  storageBucket: process.env.REACT_APP_storageBucket,
-  messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  appId: process.env.REACT_APP_appId,
-  measurementId: process.env.REACT_APP_measurementId
-};
-
-// Initialize Firebase
-const app = initializeApp(firebaseConfig);
-
-console.log('hello firebase')
+import { firebaseConfig } from './firebase-config';
 
 ReactDOM.render(
   <React.StrictMode>
@@ -47,4 +27,7 @@ ReactDOM.render(
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
 // or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+
+initializeApp(firebaseConfig);
+
 reportWebVitals();
