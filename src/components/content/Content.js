63c139ee9b23be2816c3c2c83207b22872757aff
Julio Miranda <julio.miranda12@gmail.com>
2021-10-31 00:23:12 +0100

    updating quotes

--- src/components/content/Content.js
+++ src/components/content/Content.js
@@ -1,10 +1,10 @@
-import React from "react";
-import Container from "@material-ui/core/Container";
-import { contentStyles } from "../../styles/content/styles";
+import React from 'react';
+import Container from '@material-ui/core/Container';
+import { contentStyles } from '../../styles/content/styles';
 import BizName from "./BizName";
-import BizDescription from "./BizDescription";
-import ContactInfo from "./ContactInfo";
-import Plates from "../plates/Plates";
+import BizDescription from './BizDescription';
+import ContactInfo from './ContactInfo';
+import Plates from '../plates/Plates';
 
 
 const Content = () => {
